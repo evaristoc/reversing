6d86eeecf18ea643fa89d8420e2ab05f8bfa2061
unknown <varikvi@yahoo.com>
2023-10-16 19:18:53 +0200

    additional separation of concerns - making a separate file for event handling functionalities - focusing on figure rendering

--- src/posts/2023-10-07-a-perlin-flow-field-with-canvas-shaders-and-threejs/scrollama-setup.js
+++ src/posts/2023-10-07-a-perlin-flow-field-with-canvas-shaders-and-threejs/scrollama-setup.js
@@ -1,6 +1,8 @@
-import {hello} from './huffman-flow-field-setup.js';
+//import {hello} from './huffman-flow-field-setup.js';
 import {eventHandlers} from './scrollama-eventhandlers.js';
+import {paramsFigure} from './huffman-flow-field-setup.js';
 //console.log("eventHandlers",eventHandlers);
+//console.log("paramsFigure", paramsFigure);
 
 window.onload = (event) => {
     // using d3 for convenience
@@ -42,15 +44,32 @@ window.onload = (event) => {
     }
 
     const scrolly = paramScrollama.scrolly = getElementD3js(d3, "#stickyoverlay");
-    const container = eventHandlers.container = getElementD3js(scrolly, "figure");
-    const figure = paramScrollama.figure = container;
+    paramsFigure.container = eventHandlers.container = getElementD3js(scrolly, "figure");
+    const figure = paramScrollama.figure = paramsFigure.container;
     const article = paramScrollama.article = getElementD3js(scrolly, "div .articlepost");
     const step = eventHandlers.step = getElementD3js(article, ".step", true);
-    const baseCanvas = eventHandlers.baseCanvas = getElementById(document, "context");
-    const baseContext = eventHandlers.baseContext = baseCanvas.getContext("2d");
+    const baseCanvas = document.createElement('canvas');
+    const baseContext = baseCanvas.getContext('2d');
+    const perlinCanvas = document.createElement('canvas');
+    const perlinContext = perlinCanvas.getContext('2d');
+    //const baseCanvas = getElementD3js(paramsFigure.container, "canvas");
+    //const perlinCanvas = getElementD3js(paramsFigure.container, "canvas");
+    baseCanvas.setAttribute("id", "context");
+    perlinCanvas.setAttribute("id", "perlinCanvas");
+    paramsFigure.container.node().appendChild(baseCanvas);
+    paramsFigure.baseCanvas = eventHandlers.baseCanvas = getElementById(document, "context");
+    paramsFigure.baseContext = eventHandlers.baseContext = baseContext;
+    paramsFigure.perlinCanvas = getElementById(document, "perlinCanvas");
+    paramsFigure.perlinContext = perlinContext;
+    //paramsFigure.renderer.setSize( 300, 200 );
+    paramsFigure.width = paramsFigure.container.node().offsetWidth;
+    paramsFigure.height = 200;
+    paramsFigure.renderingFunc.startTime = paramsFigure.startTime;
+    
 
-    var width;
-    var height;
+
+    //var width;
+    //var height;
 
     // initialize the scrollama
     var scroller = scrollama();
@@ -90,19 +109,32 @@ window.onload = (event) => {
         
         handleResize();
 
-    // 2. setup the scroller passing options
-        // 		this will also initialize trigger observations
+        //paramsFigure.renderFunc.renderer = paramsFigure.renderer;
+        //requestAnimationFrame(paramsFigure.render);
+        //paramsFigure.renderingFunc.render();
+        function figRender(){
+            let now = new Date().getTime();
+            let startTime = paramsFigure.startTime;
+            let currentTime = (now - startTime)/10000;
+            //console.log(currentTime);
+            requestAnimationFrame( figRender );
+        }
         
-    // 3. bind scrollama event handlers (this can be chained like below)
-    
-    scroller.setup({
-            step: '#stickyoverlay div.articlepost .step',
-            offset: .33,
-            debug: false,
-        })
-            .onStepEnter(handleStepEnter);
+        figRender();
+
+        // 2. setup the scroller passing options
+            // 		this will also initialize trigger observations
+            
+        // 3. bind scrollama event handlers (this can be chained like below)
         
-    // setup resize event
+        scroller.setup({
+                step: '#stickyoverlay div.articlepost .step',
+                offset: .33,
+                debug: false,
+            })
+                .onStepEnter(handleStepEnter);
+            
+        // setup resize event
         window.addEventListener('resize', handleResize);
     
     
