83103d9b93a06a08de1bfd1fce914bfef1333936
unknown <varikvi@yahoo.com>
2024-06-12 12:28:16 +0200

    post: new post (TSL part 2)

--- mngassets/posts/2024-05-27-tls-ssl-v1.2-handshake-made-simple-session-keys-generation/js/scrollama-setup.js
+++ mngassets/posts/2024-05-27-tls-ssl-v1.2-handshake-made-simple-session-keys-generation/js/scrollama-setup.js
@@ -61,198 +61,147 @@ window.onload = (event) => {
     //   })(document.getElementById('scroll'));
 
     //console.log(document.querySelectorAll('div.step'))
-    (function(divsteps){
-        console.log(divsteps);
-        for(let i = 0; i < divsteps.length; i++){
-            if(divsteps.classList?.contains('is-active')){
-                console.log(divsteps.classList[i]);
-                break;
-            }
-        }
-    })(document.querySelectorAll('div.step'))
-
-
-    //   (function(scroll){
-    //     scroll.onscroll = function(){
-    //         let sibling = scroll.parentNode.querySelector('.arrow');
-    //         //console.log(sibling.style.opacity);
-    //         //console.log(scroll.parentNode.style.height, scroll.parentNode.getBoundingClientRect(), scroll.scrollTop, scroll.clientHeight, scroll.scrollHeight);  
-    //         //sibling.style.opacity = 1 - (scroll.scrollTop + scroll.clientHeight)/scroll.scrollHeight;
-    //         //console.log(scroll.scrollTop, scroll.clientHeight, scroll.scrollHeight);
-    //         sibling.style.opacity = 1 - scroll.scrollTop/(scroll.scrollHeight - scroll.clientHeight)             
-    //         if (scroll.scrollTop + scroll.clientHeight == scroll.scrollHeight) {
-    //             //console.log('hither!');
+    // (function(divsteps){
+    //     console.log(divsteps);
+    //     for(let i = 0; i < divsteps.length; i++){
+    //         if(divsteps.classList?.contains('is-active')){
+    //             console.log(divsteps.classList[i]);
+    //             break;
     //         }
-    //         }
-    //   })(document.querySelector('div.step'));
+    //     }
+    // })(document.querySelectorAll('div.step'))
 
-    //   (function(smoothScroll){
-    //     console.log(smoothScroll);
-    //   })(document.getElementsByClassName('is-active'));
+
+    const resourcesImgs = '../../../../../mngassets/posts/2024-05-27-tls-ssl-v1.2-handshake-made-simple-session-keys-generation/img/';
+
+    let imageNamesObj = {
+         0: {img:null,type:'t',text:true},
+         1: {img:'TLS - asymmetric keys.png',type:'e',text:true},
+         2: {img:'TLS - client asymmetric encryption.svg',type:'e',text:true},
+         3: {img:'TLS - clientPREMASTER2server airplane.svg',type:'i',text:false},
+         4: {img:'TLS - server asymmetric decryption.svg',type:'e',text:true},
+         5: {img:null,type:'t',text:true},
+         6: {img:'TLS - randoms and pre-master.png',type:'e',text:true},
+         7: {img:'TLS - PRF Master Secret.svg',type:'e',text:true},
+         8: {img:'TLS - randoms and master.png',type:'e',text:true},
+         9: {img:'TLS - pre-master or master.png',type:'e',text:true},
+         10: {img:'TLS - PRF Session Keys.svg',type:'e',text:true},
+         11: {img:null,type:'t',text:true},
+         12: {img:'TLS - session keys.png',type:'e',text:true},
+         13: {img:'TLS - session keys.png',type:'e',text:true},
+         14: {img:'TLS - clientFINISHED2server airplane.svg',type:'i',text:false},
+         15: {img:null,type:'e',text:true},
+         16: {img:'TLS - serverFINISHED2client airplane.svg',type:'i',text:false},
+         17: {img:null,type:'e',text:true}
+    }
+
 
     // scrollama event handlers
     function handleStepEnter(response) {
         //console.log(response)
         // response = { element, direction, index }
         // add color to current step only
+        // E: add class active to the step which response.index is entering
         step.classed('is-active', function (d, i) { return i === response.index; });
             //console.log('response', response);
             //response.element.querySelector('.explain').style.display = 'inline';
-    // update graphic based on step
+        // update graphic based on step
         //figure.select('p').text(response.index + 1);
         let figElem = document.getElementById('scrollfig');
 
-        const resourcesImgs = '../../../../../mngassets/posts/2024-05-27-tls-ssl-v1.2-handshake-made-simple-session-keys-generation/img/';
-
-        let skipExplain = [0,5,11,15,17];
-
-        if(skipExplain.includes(response.index)){
+        function imagePlacing(imageName){
             figElem.innerHTML = "";
-        }
-        else if(response.index == 1){
-            figElem.innerHTML = "";
-            let img = document.createElement("img");
-            img.src = resourcesImgs + 'TLS - asymmetric keys.png';
-            img.classList.add('scrollamaImg');
-            figElem.appendChild(img);
-
-        }
-        else if(response.index == 2){
-            //let scrolly = document.querySelector('#scrolly');
-            //let a = scrolly.querySelector('figure');
-            //a.style.flexBasis = '0%';
-            figElem.innerHTML = "";
-            let img = document.createElement("img");
-            img.src = resourcesImgs + 'TLS - client asymmetric encryption.svg';
-            img.classList.add('scrollamaImg');
-            figElem.appendChild(img);            
-        }
-        else if(response.index == 3){
-            figElem.innerHTML = "";
-            //let step4Elem = document.querySelector(".step[data-step='4']");
-            //step4Elem.style.width = '1px';
-            //step4Elem.parentNode.style.width = '0px';
-            //let scrolly = document.querySelector('#scrolly');
-            //let a = scrolly.querySelector('figure');
-            //a.style.flexBasis = '80%';
-            //let b = scrolly.querySelector('articlepost');
-            //b.style.flexBasis = '20%';
-            let img = document.createElement("img");
-            img.src = resourcesImgs + 'TLS - clientPREMASTER2server airplane.svg';
-            img.classList.add('scrollamaImg');
-            figElem.appendChild(img);            
-        }
-        else if(response.index == 4){
-            //let scrolly = document.querySelector('#scrolly');
-            //let a = scrolly.querySelector('figure');
-            //a.style.flexBasis = '0%';
-            figElem.innerHTML = "";
-            let img = document.createElement("img");
-            img.src = resourcesImgs + 'TLS - server asymmetric decryption.svg';
-            img.classList.add('scrollamaImg');
-            figElem.appendChild(img);            
-        }
-        else if(response.index == 6){
-            figElem.innerHTML = "";
-            let img = document.createElement("img");
-            img.src = resourcesImgs + 'TLS - randoms and pre-master.png';
-            img.classList.add('scrollamaImg');
-            figElem.appendChild(img);            
-        }
-        else if(response.index == 7){
-            figElem.innerHTML = "";
-            let img = document.createElement("img");
-            img.src = resourcesImgs + 'TLS - PRF Master Secret.svg';
-            img.classList.add('scrollamaImg');
-            figElem.appendChild(img);            
-        }
-        else if(response.index == 8){
-            figElem.innerHTML = "";
-            let img = document.createElement("img");
-            img.src = resourcesImgs + 'TLS - randoms and master.png';
-            img.classList.add('scrollamaImg');
-            figElem.appendChild(img);            
-        }
-        else if(response.index == 9){
-            figElem.innerHTML = "";
-            let img = document.createElement("img");
-            img.src = resourcesImgs + 'TLS - pre-master or master.png';
-            img.classList.add('scrollamaImg');
-            figElem.appendChild(img);            
-        }
-        else if(response.index == 10){
-            figElem.innerHTML = "";
-            let img = document.createElement("img");
-            img.src = resourcesImgs + 'TLS - PRF Session Keys.svg';
-            img.classList.add('scrollamaImg');
-            figElem.appendChild(img);            
-        }
-        else if(response.index == 12 && figElem.getElementsByClassName('scrollamaImg')[0] == null){
-            figElem.innerHTML = "";
-            let img = document.createElement("img");
-            img.src = resourcesImgs + 'TLS - session keys.png';
-            img.classList.add('scrollamaImg');
-            figElem.appendChild(img);
-        }
-        else if(response.index == 13){
-            console.log(response.element.querySelector('.explain'));
-            (function(scroll){
-                scroll.onscroll = function(){
-                    let sibling = scroll.parentNode.querySelector('.arrow');
-                    sibling.style.opacity = 1 - scroll.scrollTop/(scroll.scrollHeight - scroll.clientHeight);
-                }
-            })(response.element.querySelector('.explain'));
-            
-            response.element.querySelector('.arrow')
-
-            response.element.querySelector('.arrow').onclick = function(){
-                let sibling = response.element.querySelector('.explain');
-                console.log(response.element);
-                sibling.scrollBy({
-                    top:10,
-                    left:0,
-                    behaviour:'smooth'
-                });
-            };
-            
-            if(!figElem.getElementsByClassName('scrollamaImg')[0].src.includes('TLS%20-%20session%20keys.png')){
-                //response.index == 13 && figElem.getElementsByClassName('scrollamaImg')[0] == null
-                //console.log(figElem.getElementsByClassName('scrollamaImg')[0].src);
-                figElem.innerHTML = "";
+            if(imageName){
                 let img = document.createElement("img");
-                img.src = resourcesImgs + 'TLS - session keys.png';
+                img.src = resourcesImgs + imageName;
                 img.classList.add('scrollamaImg');
                 figElem.appendChild(img);
             }
-        } 
+       }
+   
+        function arrowOpacityAnimationScroll(){
+            let scroll = response.element.querySelector('.explain');
+            //if(response.element.querySelector('.explain').style.height > '50px'){
+                scroll.onscroll = function(){
+                    let arrowDown = scroll.parentNode.querySelector('.arrow.down');
+                    arrowDown.style.opacity = 1 - scroll.scrollTop/(scroll.scrollHeight - scroll.clientHeight);
+                    let arrowTop = scroll.parentNode.querySelector('.arrow.top');
+                    arrowTop.style.opacity = scroll.scrollTop/(scroll.scrollHeight - scroll.clientHeight);
+                }
+            //}else{
+            //    response.element.querySelectorAll('.arrow-wrap .arrow').forEach((v)=>{console.log(v);v.style.display = 'none';}) 
+            //}
 
-    // (function(scroll){
-    //     scroll.onscroll = function(){
-    //         let sibling = scroll.parentNode.querySelector('.arrow');
-    //         //console.log(sibling.style.opacity);
-    //         //console.log(scroll.parentNode.style.height, scroll.parentNode.getBoundingClientRect(), scroll.scrollTop, scroll.clientHeight, scroll.scrollHeight);  
-    //         //sibling.style.opacity = 1 - (scroll.scrollTop + scroll.clientHeight)/scroll.scrollHeight;
-    //         //console.log(scroll.scrollTop, scroll.clientHeight, scroll.scrollHeight);
-    //         sibling.style.opacity = 1 - scroll.scrollTop/(scroll.scrollHeight - scroll.clientHeight)             
-    //         if (scroll.scrollTop + scroll.clientHeight == scroll.scrollHeight) {
-    //             //console.log('hither!');
-    //         }
-    //         }
-    //   })(document.getElementById('scroll'));
-        else if(response.index == 14){
-            figElem.innerHTML = "";
-            let img = document.createElement("img");
-            img.src = resourcesImgs + 'TLS - clientFINISHED2server airplane.svg';
-            img.classList.add('scrollamaImg');
-            figElem.appendChild(img);                
+       };
+   
+   
+       function arrowAnimationClick(){
+           response.element.querySelector('.arrow.down').onclick = function(){
+               let sibling = response.element.querySelector('.explain');
+               sibling.scrollBy({
+                   top:10,
+                   left:0,
+                   behaviour:'smooth'
+               });
+           };
+           response.element.querySelector('.arrow.top').onclick = function(){
+               let sibling = response.element.querySelector('.explain');
+                sibling.scrollBy({
+                   top:-10,
+                   left:0,
+                   behaviour:'smooth'
+               });
+           };    
+       }
+
+        try{
+            if(response.index in imageNamesObj){
+                let currentActive = imageNamesObj[response.index];
+                if([0,1,2,3,4,5,6,7,8,9,10,11,14,15,16,17].includes(response.index)){
+                    imagePlacing(currentActive.img);
+                    if(currentActive.text && currentActive.type == 'e'){
+                        let totalHeight = 0;
+                        response.element.querySelector('.explain').querySelectorAll('p').forEach((v)=>{totalHeight += v.clientHeight;})
+                        if(totalHeight > .97*response.element.querySelector('.explain').clientHeight){
+                            arrowOpacityAnimationScroll();
+                            arrowAnimationClick();
+                        }else{
+                            response.element.querySelectorAll('.arrow-wrap').forEach((v)=>{v.style.display = 'none';})
+                        }
+                    }
+                }else if(response.index == 12 && figElem.getElementsByClassName('scrollamaImg')[0] == null){
+                    imagePlacing(currentActive.img);
+                    let totalHeight = 0;
+                    response.element.querySelector('.explain').querySelectorAll('p').forEach((v)=>{totalHeight += v.clientHeight;})
+                    if(totalHeight > .97*response.element.querySelector('.explain').clientHeight){
+                        arrowOpacityAnimationScroll();
+                        arrowAnimationClick();
+                    }else{
+                        response.element.querySelectorAll('.arrow-wrap').forEach((v)=>{v.style.display = 'none';})
+                    }
+                }
+                else if(response.index == 13){
+                    let totalHeight = 0;
+                    response.element.querySelector('.explain').querySelectorAll('p').forEach((v)=>{totalHeight += v.clientHeight;})
+                    //console.log(total, Array.from(response.element.querySelector('.explain').querySelectorAll('p')).reduce((t,p)=>{return t += p.clientHeight;}), .95*response.element.querySelector('.explain').clientHeight);
+                    if(totalHeight > .97*response.element.querySelector('.explain').clientHeight){
+                        arrowOpacityAnimationScroll();
+                        arrowAnimationClick();
+                    }else{
+                        response.element.querySelectorAll('.arrow-wrap').forEach((v)=>{v.style.display = 'none';})
+                    }
+                    if(!figElem.getElementsByClassName('scrollamaImg')[0].src.includes('TLS%20-%20session%20keys.png')){
+                        imagePlacing(currentActive.img);
+                    }
+                }
+            }else{
+                alert('step not found');
+            }
+        }catch(err){
+            alert(err.message);
+            document.getElementById("error").innerHTML = err.message;          
         }
-        else if(response.index == 16){
-            figElem.innerHTML = "";
-            let img = document.createElement("img");
-            img.src = resourcesImgs + 'TLS - serverFINISHED2client airplane.svg';
-            img.classList.add('scrollamaImg');
-            figElem.appendChild(img);                
-        }
+
     }
 
     //E: RELEVANT - it is a different library to stick the menu; scrollama doesn't handle this!
